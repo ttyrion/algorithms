89e1825ecf01b8168cf6299945dc41e3638d564a
ttyrion <tyrion.marting@hotmail.com>
2018-12-01 22:33:53 +0800

    kmp

--- strfind.cpp
+++ strfind.cpp
@@ -1,68 +1,10 @@
 #include "strfind.h"
 #include <string.h>
 #include <functional>
+#include <iostream>
 
 namespace algorithm {
     namespace find{
-        /**
-         * @next_array partly initialized array of next, 
-         *             of which the numbers of elements should be no less than the length of pattern
-         * @size       numbers of elements in next_array
-         * @j          the index of pattern of which we want to get the next value
-         * @pattern    the pattern string we want to find
-         * @return     the next value of j
-        */
-       #ifdef ZERO
-        int get_next(int *next_array, int j, char *pattern) {
-            const int pattern_len = strlen(pattern);
-            /* next[0] should always be -1, it makes the pattern moves one position when missing match*/
-            if(j == 0 || j >= pattern_len) {
-                return -1;
-            }
-
-            if(pattern[next_array[j-1]] == pattern[j-1]) {
-                return next_array[j-1] + 1;
-            }
-            else {
-                
-            }
-        }
-
-        int fill_next(int *next_array, int size, char *pattern) {
-            for(int j = 0; j < size; ++j) {
-
-            }   
-        }
-
-        int kmp_find_first_of(char *str, char *pattern) {
-            const int str_len = strlen(str);
-            const int pattern_len = strlen(pattern);
-
-            int *next = new int[pattern_len];
-            memset(next,0,sizeof(int) * pattern_len);
-            
-
-            int i = 0, j = 0;
-            for(; i < str_len && j < pattern_len;) {
-                if(str[i] == pattern[j]) {
-                    ++i;
-                    ++j;
-                }
-                else {
-                    /* keep i unchanged, change j */
-                    j = kmp_next(j);
-                }
-            }
-
-            if(j == pattern_len) {
-                return i;
-            }
-
-            return -1;
-        }
-
-        #endif
-
         int* kmp_get_next(const char *p) {
             const int pn = strlen(p);
             if(pn <= 0) {
@@ -71,49 +13,49 @@ namespace algorithm {
 
             int *next = new int[pn];
             next[0] = -1;
-
             memset(next+1, 0, sizeof(int)*(pn-1));
 
-            /* pi must >= 2. */
-            std::function<unsigned int (unsigned int)> get_next;
-            get_next = [next,p,&get_next](unsigned int pi) -> unsigned int {
-                if(pi <= 0) {
-                    return -1;
+            for(int pi = 1; pi < pn; ++pi) {
+                int k = next[pi-1];
+                while(true) {
+                    if (k == -1 || p[k] == p[pi-1]) {
+                        next[pi] = k + 1;
+                        break;
+                    }
+                    else {
+                        k = next[k];
+                    }
                 }
-
-                int m = next[pi-1];
-
-                if(p[(next[pi-1])] == p[pi-1]) {
-                    return next[pi-1] + 1;
-                }
-                else {
-                    return get_next(next[pi-1]) + 1;
-                }
-            };
-
-            for(int i = 2; i < pn; ++i) {
-                next[i] = get_next(i);
             } 
+
+            return next;
         }
 
         int kmp_find_first_of(const char *str, const char *pattern) {
             const int sn = strlen(str);
             const int pn = strlen(pattern);
+            int *next = kmp_get_next(pattern);
             int si = 0;
-            for(; si <= sn - pn; ++si) {
+            for(; si <= sn - pn;) {
                 int pi = 0;
-                for(; pi < pn; ++pi) {
+                for(; pi < pn;) {
                     if(str[si+pi] != pattern[pi]) {
+                        /*Changing pi actually moves si.*/
+                        si += pi - next[pi];
+                        pi = next[pi];
                         break;
                     }
+                    ++pi;
                 }
 
                 /* match */
                 if (pi == pn) {
+                    delete next;
                     return si;
                 }
             }
 
+            delete next;
             return -1;
         }
     }
--- strfind.h
+++ strfind.h
@@ -1,11 +1,16 @@
 namespace algorithm {
     namespace find{
         /**
-         * @p  the pattern string when doing string match
+         * @p  the pattern string to be matched.
          * @return the next array of the pattern string p, 
-         *         it should be deleted out of use if not null.
+         *         it should be deleted after being used if not null.
         */
         int* kmp_get_next(const char *p);
+
+        /**
+         * @str the source string in which we want to match the pattern.
+         * @pattern the pattern string to be matched.
+        */
         int kmp_find_first_of(const char *str, const char *pattern);
     }
 }
